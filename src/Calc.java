96d7bd490c34f0514c601c88c77d45ce68b64434
galina <galina.krupitsa@gmail.com>
2025-08-10 10:45:24 -0700

    update

--- src/Calc.java
+++ src/Calc.java
@@ -1,10 +1,14 @@
 public class Calc {
-    static  double add(double a, double b) {
+    static double add(double a, double b) {
         return a + b;
     }
-    static double division(double a,double b)throws  ArithmeticException
-    {
-        return a / b;
-        }
+
+static double division(double a, double b) {
+    if (b == 0) {
+        System.out.println("Деление на ноль");
+        return Double.NaN;
     }
+    return a / b;
+}
+}
 
--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Калькулятор");
         double a = 5;
-        double b = 6;
-        Calc w = new Calc();
-        System.out.println(w.division(a,b));
+        double b = 0;
+//        Calc w = new Calc();
+        System.out.println(a + " / " + b + " = "+ Calc.division(a,b));
+        System.out.println(a + " + " + b + " = "+ Calc.add(a,b));
     }
 
 
