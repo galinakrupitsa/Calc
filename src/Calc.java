18a9a6582534da71207064da5298c0833b2c28ab
galina <galina.krupitsa@gmail.com>
2025-08-10 06:26:52 -0700

    update

---
+++ src/Calc.java
@@ -0,0 +1,10 @@
+public class Calc {
+    static  double add(double a, double b) {
+        return a + b;
+    }
+    static double division(double a,double b)throws  ArithmeticException
+    {
+        return a / b;
+        }
+    }
+
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        double a = 5;
+        double b = 6;
+        Calc w = new Calc();
+        System.out.println(w.division(a,b));
     }
 
 
